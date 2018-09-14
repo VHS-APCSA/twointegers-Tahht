2e7bcfd90b8e7ff1c24a9aaffe7500b62e0a73b5
bob <bob@APCSA>
2018-09-14 15:37:37 -0700

    Work in progress

---
+++ src/TwoIntRunner.java
@@ -0,0 +1,10 @@
+
+public class TwoIntRunner {
+	
+	public static void main(String[] args)
+	{
+		TwoIntegers twoIntegers = new TwoIntegers(3, 5);
+		System.out.println(twoIntegers.arithmetic());
+		
+	}
+}
---
+++ src/TwoIntegers.java
@@ -0,0 +1,47 @@
+
+public class TwoIntegers {
+
+	private int num;
+	private int num2;
+	
+	public TwoIntegers(int num, int num2)
+	{
+		this.num = num;
+		this.num2 = num2;
+	}
+	
+	
+	public TwoIntegers()
+	{
+		num = 0;
+		num2 = 0;
+	}
+	public int getNum()
+	{
+		return num;
+	}
+	public int getNum2()
+	{
+		return num2;
+	}
+	public void setNum(int num)
+	{
+		this.num = num;
+	}
+	public void setNum2(int num2)
+	{
+		this.num2 = num2;
+	}
+	public String arithmetic()
+	{
+		int numsum = num + num2;
+		int numproduct = num * num2;
+		int numdifference = num - num2;
+		int numquotient = num / num2;
+		
+		return num + " + " + num2 + " = " + numsum + 
+				", " + num + " * " + num2 + " = " + numproduct;
+		
+	}
+	public String 
+}
