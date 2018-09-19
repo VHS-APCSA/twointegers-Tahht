074144985ff245c79db28321b757c46b1c433647
bob <bob@APCSA>
2018-09-19 14:55:28 -0700

    Work in progress

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -4,34 +4,38 @@ public class TwoIntegers {
 	private int num;
 	private int num2;
 	
+	public TwoIntegers()
+	{
+		num = 0;
+		num2 = 0;
+	}
+	
 	public TwoIntegers(int num, int num2)
 	{
 		this.num = num;
 		this.num2 = num2;
 	}
 	
-	
-	public TwoIntegers()
-	{
-		num = 0;
-		num2 = 0;
-	}
 	public int getNum()
 	{
 		return num;
 	}
+	
 	public int getNum2()
 	{
 		return num2;
 	}
+	
 	public void setNum(int num)
 	{
 		this.num = num;
 	}
+	
 	public void setNum2(int num2)
 	{
 		this.num2 = num2;
 	}
+	
 	public String arithmetic()
 	{
 		int numsum = num + num2;
@@ -43,5 +47,18 @@ public class TwoIntegers {
 				", " + num + " * " + num2 + " = " + numproduct;
 		
 	}
-	public String 
+	public int larger()
+	{
+		if(num >= num2)
+			return num;
+		else
+			return num2;
+	}
+	
+	public boolean isEven()
+	{
+		if(num + num2 = 2 % ==0)
+			return true;
+	}
+	
 }
